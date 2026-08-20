07b4b8c69d77069dc150ddf9ca1766cb79fc92d3
VoVanCuong <cuongcon2k04@gmail.com>
2026-08-20 08:33:54 +0700

    chore:update file codeIEmbeddingProvider.cs, ILLMProvider.cs

--- AIArchitectureReviewer/AIArchitectureReviewer.Application/Interfaces/AI/IEmbeddingProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace AIArchitectureReviewer.Application.Interfaces.AI
-{
-    public interface IEmbeddingProvider
-    {
-        Task<float[]> GetEmbeddingAsync(string text);
-    }
-}
